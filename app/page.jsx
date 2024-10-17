a7dfc93f112304912a41c6028a9d0ac1e122be21
DurveshMore8 <durveshmore2003@gmail.com>
2024-10-17 06:51:17 +0530

    added a home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h2>Hello</h2>
-      <Button>Subscribe</Button>
+    <div className="h-[100vh] flex justify-center items-center gap-6 flex-col">
+      <h2 className="font-bold text-3xl">Welcome to AI Interview Bot</h2>
+      <Link href={"/dashboard"}>
+        <Button>Get Started</Button>
+      </Link>
     </div>
   );
 }
